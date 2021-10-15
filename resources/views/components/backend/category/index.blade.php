e39291c507e5df1c7fe1d7910012440411f61a3f
khanhnam99 <khanhnam99@gmail.com>
2021-10-15 16:06:58 +0700

    Update

--- resources/views/components/backend/category/index.blade.php
+++ resources/views/components/backend/category/index.blade.php
@@ -31,8 +31,8 @@
                                             <tr>
                                                 <th scope="col" class="width-5-percent">#</th>
                                                 <th scope="col">Name</th>
-                                                <th scope="col" class="width-15-percent">Status</th>
                                                 <th scope="col" class="width-15-percent">Create Date</th>
+                                                <th scope="col" class="width-15-percent">Status</th>
                                                 <th scope="col" class="width-20-percent">Functions</th>
                                             </tr>
                                             </thead>
@@ -42,6 +42,7 @@
                                                     <tr>
                                                         <th scope="row">{{ $offset }}</th>
                                                         <td>{{ $item->name }}</td>
+                                                        <td>{{ !empty($item->created_at) ? date('d/m/Y',strtotime($item->created_at)) : '' }}</td>
                                                         <td>
                                                             @if($item->status == 1)
                                                                 <span class="badge badge-style-bordered badge-success">Active</span>
@@ -49,7 +50,6 @@
                                                                 <span class="badge badge-style-bordered badge-warning">Deactivated</span>
                                                             @endif
                                                         </td>
-                                                        <td>{{ !empty($item->created_at) ? date('d/m/Y',strtotime($item->created)) : '' }}</td>
                                                         <td>
                                                             <a  class="btn btn-primary btn-sm"><i class="material-icons">create</i> Edit </a>
                                                             <a  class="btn btn-secondary btn-sm deleteAction"  data-id="{{ $item->id }}"><i class="material-icons">delete_outline</i> Delete</a>
